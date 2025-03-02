96ae650bddc98db7bc2ac6991e01202a01601a5d
Tchamo Elinor <tchamoelii@gmail.com>
2025-03-02 14:33:35 +0100

    Feat: Responsive navbar, header and social part

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Link } from './link';
-import test from 'node:test';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { hugeMenu01 } from '@ng-icons/huge-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  viewProviders: provideIcons({ hugeMenu01 }),
 })
 export class NavbarComponent {
   links: Link[] = [
