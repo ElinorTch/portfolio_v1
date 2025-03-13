93cea84427e28b0983144e1cd5138bd9efa07b53
Tchamo Elinor <tchamoelii@gmail.com>
2025-03-13 17:37:05 +0100

    Feat: mobile nav ongoing

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -2,32 +2,32 @@ import { Component } from '@angular/core';
 import { Link } from './link';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { hugeMenu01 } from '@ng-icons/huge-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgIcon],
+  imports: [NgIcon, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   viewProviders: provideIcons({ hugeMenu01 }),
 })
 export class NavbarComponent {
+  show: boolean = false;
+
+  showNavbar(): void {
+    this.show = !this.show;
+    document.body.classList.toggle('overflow-hidden', this.show);
+  }
+
   links: Link[] = [
     {
       name: 'Accueil',
-      target: '',
+      target: '#header',
     },
     {
       name: 'Expériences',
-      target: 'experiences',
-    },
-    {
-      name: 'Compétences',
-      target: '',
-    },
-    {
-      name: 'Contact',
-      target: '',
+      target: '#experiences',
     },
   ];
 }
